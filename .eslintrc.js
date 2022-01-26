e6fff00bfd7deb2271c8bb85a89d1a2a9bea39ae
Grey Hixson <greyhixson@gmail.com>
2022-01-25 20:59:46 -0600

    Init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
   },
+
 };
