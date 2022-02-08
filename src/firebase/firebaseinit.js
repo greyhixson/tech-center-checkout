2cfbcc32e686c7c6745d6767e9032d7e49cb8b4f
Grey Hixson <greyhixson@gmail.com>
2022-02-07 20:18:16 -0600

    init firebase config

---
+++ src/firebase/firebaseinit.js
@@ -0,0 +1,19 @@
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWz4NdhSuvxA-pOEEr4kCFFDRRvFKt9S0',
+  authDomain: 'tech-center-checkout.firebaseapp.com',
+  projectId: 'tech-center-checkout',
+  storageBucket: 'tech-center-checkout.appspot.com',
+  messagingSenderId: '913017175437',
+  appId: '1:913017175437:web:453f659b92e3b14af391c4',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
