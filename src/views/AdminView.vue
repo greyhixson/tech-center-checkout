d9ee8ac9d4a89123a2ad383bf9aa5a5b5016f1df
Grey Hixson <greyhixson@gmail.com>
2022-02-15 13:11:15 -0600

    Setup initial files

---
+++ src/views/AdminView.vue
@@ -0,0 +1,15 @@
+<template>
+  <admin-dashboard />
+</template>
+
+<script>
+import AdminDashboard from '../components/Admin/AdminDashboard.vue';
+
+export default {
+  name: 'AdminView',
+
+  components: {
+    AdminDashboard,
+  },
+};
+</script>
---
+++ src/views/UserView.vue
@@ -0,0 +1,15 @@
+<template>
+  <user-home />
+</template>
+
+<script>
+import UserHome from '../components/User/UserHome.vue';
+
+export default {
+  name: 'UserView',
+
+  components: {
+    UserHome,
+  },
+};
+</script>
