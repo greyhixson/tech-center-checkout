c12afc7f2d98d0eb0ea99233ef4d073f047e3146
Kendra Risener <risenerkendra@gmail.com>
2022-02-15 23:32:24 -0600

    Sign-out button, page header, link to user profile/settings page

--- src/components/Admin/AdminDashboard.vue
+++ src/components/Admin/AdminDashboard.vue
@@ -1,19 +1,41 @@
+/* eslint-disable vue/valid-template-root */
+/* eslint-disable vue/valid-template-root */
 <template>
-  <v-container>
-    <v-card
-      elevation="6"
-      outlined
-      max-width="500"
+  <v-card
+    class="grey lighten-4"
+    max-width="2500"
+    style="height: 100%;"
+    fluid
+  >
+    <v-system-bar />
+    <v-toolbar
+      flat
+      height="75%"
     >
-      <template slot="progress">
-        <v-progress-linear
-          color="deep-purple"
-          height="10"
-          indeterminate
-        />
-      </template>
-    </v-card>
-  </v-container>
+      <v-toolbar-title>
+        Welcome,
+        <a
+          href="/#/Profile"
+          class="text-decoration-none"
+        >
+          Admin
+        </a>
+      </v-toolbar-title>
+      <v-btn
+        depressed
+        :right="true"
+        :absolute="true"
+      >
+        Sign Out
+      </v-btn>
+      <v-spacer />
+    </v-toolbar>
+    <v-banner
+      single-line
+      :sticky="sticky"
+      height="75%"
+    />
+  </v-card>
 </template>
 
 <script>
