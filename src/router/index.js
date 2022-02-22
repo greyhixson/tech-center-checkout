93cb70204fb5002a754683ac86aa65bd125a5e21
Grey Hixson <greyhixson@gmail.com>
2022-02-22 13:01:53 -0600

    Revert "PR for admin-dashboard-init"

--- src/router/index.js
+++ src/router/index.js
@@ -16,11 +16,6 @@ const routes = [
     name: 'AdminView',
     component: AdminView,
   },
-  {
-    path: '/Profile',
-    name: 'Profile',
-    // add component later
-  },
 ];
 
 const router = new VueRouter({
