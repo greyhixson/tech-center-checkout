2e9773c2750481cd72319220db664a5157d1e484
Grey Hixson <greyhixson@gmail.com>
2022-02-22 13:20:44 -0600

    remade without need for npm install

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// https://vuetifyjs.com/en/features/icon-fonts/#material-icons: follow instructions to download material icon package
-
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify);
 
