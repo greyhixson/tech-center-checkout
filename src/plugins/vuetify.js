c06255025f92e002e7f5ce3c7dfd22aa5b3c64a4
Grey Hixson <greyhixson@gmail.com>
2022-02-22 18:12:25 -0600

    even more fixed

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// https://vuetifyjs.com/en/features/icon-fonts/#material-icons: follow instructions to download material icon package
-
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
