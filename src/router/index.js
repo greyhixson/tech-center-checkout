5783b41cb4cb15a079e55e5cdb2ca987cbce6721
Dianne Perez <dp023@uark.edu>
2022-02-26 17:24:49 -0600

    Comments added to scripts

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import AdminView from '../views/AdminView.vue';
+import AdminActive from '../components/Admin/AdminActive.vue'; // imported from components-Admin Active Reservations
+import AdminLog from '../components/Admin/AdminLog.vue'; // imported from components-Admin Logged Reservations
 import UserView from '../views/UserView.vue';
 
 Vue.use(VueRouter);
@@ -20,6 +22,16 @@ const routes = [
     path: '/Profile',
     name: 'Profile',
     // add component later
+  },
+  {
+    path: '/AdminActiveRes',
+    name: 'AdminActiveRes',
+    component: AdminActive, // imported component
+  },
+  {
+    path: '/AdminLogRes',
+    name: 'AdminLogRes',
+    component: AdminLog, // imported component
   },
 ];
 
