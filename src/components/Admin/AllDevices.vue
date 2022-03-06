bd212bfa3504ea4012743a6922833282a8553ce7
Kendra Risener <risenerkendra@gmail.com>
2022-03-06 06:52:40 -0600

    slight changes, messing around with db

--- src/components/Admin/AllDevices.vue
+++ src/components/Admin/AllDevices.vue
@@ -128,7 +128,6 @@
                     </v-row>
                   </v-container>
                 </v-card-text>
-
                 <v-card-actions>
                   <v-spacer />
                   <v-btn
@@ -148,35 +147,35 @@
                 </v-card-actions>
               </v-card>
             </v-dialog>
-            <v-dialog
-              v-model="dialogDelete"
-              max-width="500px"
-            >
-              <v-card>
-                <v-card-title class="text-h5">
-                  Are you sure you want to delete this item?
-                </v-card-title>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn
-                    color="#600000"
-                    text
-                    @click="closeDelete"
-                  >
-                    Cancel
-                  </v-btn>
-                  <v-btn
-                    color="#600000"
-                    text
-                    @click="deleteItemConfirm"
-                  >
-                    OK
-                  </v-btn>
-                  <v-spacer />
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
           </v-toolbar>
+          <v-dialog
+            v-model="dialogDelete"
+            max-width="500px"
+          >
+            <v-card>
+              <v-card-title class="text-h5">
+                Are you sure you want to delete this item?
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  color="#600000"
+                  text
+                  @click="closeDelete"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="#600000"
+                  text
+                  @click="deleteItemConfirm"
+                >
+                  OK
+                </v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </template>
         <template v-slot:[`item.actions`]="{ item }">
           <v-icon
@@ -259,6 +258,7 @@ export default {
         console.log(e);
       }
     },
+    // need async add & delete to FB collection
 
     editItem(item) {
       this.editedIndex = this.devices.indexOf(item);
