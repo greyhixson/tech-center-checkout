a511bb6aa3dafab2ace1ca653fd1f9e28ed8d193
Kendra Risener <risenerkendra@gmail.com>
2022-03-18 00:31:46 -0500

    made inventory v-table same width as banner

--- src/components/Admin/AllDevices.vue
+++ src/components/Admin/AllDevices.vue
@@ -1,157 +1,166 @@
 <template>
   <v-container
     fluid
+    class="mb-4 mt-4 lighten-4 mx-auto"
   >
-    <v-card>
-      <v-card-title>
-        <v-spacer />
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="devices"
-        :search="search"
-        class="elevation-1"
+    <v-layout
+      align-center
+      justify-center
+    >
+      <v-card
+        width="86.5%"
       >
-        <template v-slot:top>
-          <v-toolbar
-            flat
-          >
-            <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Search"
-              color="#600000"
-              single-line
-              hide-details
-              class="pr-16"
-              width="800px"
-            />
-            <v-spacer />
+        <v-card-title>
+          <v-spacer />
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="devices"
+          :search="search"
+          class="elevation-1"
+          align="center"
+        >
+          <template v-slot:top>
+            <v-toolbar
+              flat
+            >
+              <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search"
+                color="#600000"
+                single-line
+                hide-details
+                class="pr-16"
+                width="800px"
+              />
+              <v-spacer />
+              <v-dialog
+                v-model="dialog"
+                max-width="500px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="black"
+                    dark
+                    class="mb-2 mr-15"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    New Item
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="editedItem.deviceTag"
+                            label="Device Tag"
+                            color="black"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="editedItem.deviceName"
+                            label="Name"
+                            color="black"
+                          />
+                        </v-col>
+                        <v-col
+                          cols="12"
+                          sm="6"
+                          md="4"
+                        >
+                          <v-text-field
+                            v-model="editedItem.status"
+                            label="Status"
+                            color="black"
+                          />
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer />
+                    <v-btn
+                      color="#600000"
+                      text
+                      @click="close"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="#600000"
+                      text
+                      @click="save"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
             <v-dialog
-              v-model="dialog"
+              v-model="dialogDelete"
               max-width="500px"
             >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  color="black"
-                  dark
-                  class="mb-2 mr-15"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  New Item
-                </v-btn>
-              </template>
               <v-card>
-                <v-card-title>
-                  <span class="text-h5">{{ formTitle }}</span>
+                <v-card-title class="text-h5">
+                  Are you sure you want to delete this item?
                 </v-card-title>
-
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field
-                          v-model="editedItem.deviceTag"
-                          label="Device Tag"
-                          color="black"
-                        />
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field
-                          v-model="editedItem.deviceName"
-                          label="Name"
-                          color="black"
-                        />
-                      </v-col>
-                      <v-col
-                        cols="12"
-                        sm="6"
-                        md="4"
-                      >
-                        <v-text-field
-                          v-model="editedItem.status"
-                          label="Status"
-                          color="black"
-                        />
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
                 <v-card-actions>
                   <v-spacer />
                   <v-btn
                     color="#600000"
                     text
-                    @click="close"
+                    @click="closeDelete"
                   >
                     Cancel
                   </v-btn>
                   <v-btn
                     color="#600000"
                     text
-                    @click="save"
+                    @click="deleteItemConfirm"
                   >
-                    Save
+                    OK
                   </v-btn>
+                  <v-spacer />
                 </v-card-actions>
               </v-card>
             </v-dialog>
-          </v-toolbar>
-          <v-dialog
-            v-model="dialogDelete"
-            max-width="500px"
-          >
-            <v-card>
-              <v-card-title class="text-h5">
-                Are you sure you want to delete this item?
-              </v-card-title>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn
-                  color="#600000"
-                  text
-                  @click="closeDelete"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  color="#600000"
-                  text
-                  @click="deleteItemConfirm"
-                >
-                  OK
-                </v-btn>
-                <v-spacer />
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </template>
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-icon
-            small
-            class="mr-2"
-            @click="editItem(item)"
-          >
-            mdi-pencil
-          </v-icon>
-          <v-icon
-            small
-            @click="deleteItem(item)"
-          >
-            mdi-delete
-          </v-icon>
-        </template>
-      </v-data-table>
-    </v-card>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon
+              small
+              class="mr-2"
+              @click="editItem(item)"
+            >
+              mdi-pencil
+            </v-icon>
+            <v-icon
+              small
+              @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-layout>
   </v-container>
 </template>
 
