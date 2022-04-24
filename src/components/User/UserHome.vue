9b4d5c1392202c820f212d2a34675aa32ba65856
Grey Hixson <greyhixson@gmail.com>
2022-04-24 17:43:50 -0500

    updated spacing

--- src/components/User/UserHome.vue
+++ src/components/User/UserHome.vue
@@ -2,52 +2,68 @@
   <v-container
     fluid
   >
-    <v-btn
-      color="#E0E0E0"
-      class="mb-8"
-      href="/#/UserRes"
-      block
-      large
-    >
-      My Reservations
-    </v-btn>
-    <v-card>
-      <v-card-title>
-        <v-spacer />
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="devices"
-        :search="search"
-        class="elevation-1"
-      >
-        <template v-slot:top>
-          <v-toolbar
-            flat
-          >
-            <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Search"
-              color="#600000"
-              single-line
-              hide-details
-              class="pr-16"
-              width="800px"
-            />
+    <v-row justify="center">
+      <v-col cols="8">
+        <v-card>
+          <v-card-title>
             <v-spacer />
-          </v-toolbar>
-        </template>
-        <template v-slot:[`item.actions`]="{ item }">
-          <v-btn
-            href="/#/UserReviewRes"
-            @click="selectItem(item)"
+          </v-card-title>
+          <v-data-table
+            :headers="headers"
+            :items="devices"
+            :search="search"
+            class="elevation-1"
           >
-            Reserve
-          </v-btn>
-        </template>
-      </v-data-table>
-    </v-card>
+            <template v-slot:top>
+              <v-toolbar
+                flat
+              >
+                <v-row align="center">
+                  <v-col
+                    cols="6"
+                    class="grow pl-4"
+                  >
+                    <v-text-field
+                      v-model="search"
+                      append-icon="mdi-magnify"
+                      label="Search"
+                      color="#600000"
+                      single-line
+                      hide-details
+                      class="pr-16"
+                      width="800px"
+                    />
+                  </v-col>
+                  <v-spacer />
+                  <v-col
+                    cols="auto"
+                    class="mt-4"
+                    style="padding-right: 120px;"
+                  >
+                    <v-btn
+                      color="#E0E0E0"
+                      class="mb-8"
+                      href="/#/UserRes"
+                      large
+                    >
+                      My Reservations
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-toolbar>
+            </template>
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-btn
+                href="/#/UserReviewRes"
+                @click="selectItem(item)"
+              >
+                Reserve
+              </v-btn>
+            </template>
+          </v-data-table>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
